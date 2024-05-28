2df96a82fad77a665cb48210a90f7e75a66b5974
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2024-05-28 15:33:29 -0300

    Challenge: Bank account

---
+++ conta-banco-desafio/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o valor que deseja sacar: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+        
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+        ", conta " + numero + " e seu saldo " + saldo + " já esta disponível para saque.");
+    }
+}
